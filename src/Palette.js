cf9aaddb9c5306d4c2506dd2c09cd827bfa636ce
Lilly Oh <ohlilly@gmail.com>
2020-04-07 12:14:03 -0400

    created app, seed file, Palette component

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Palette extends React.Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
