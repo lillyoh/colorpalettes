00d283d129ac4134b4a759544183dd2e995511fd
Lilly Oh <ohlilly@gmail.com>
2020-04-08 20:52:25 -0400

    added remove icon and ability to remove colors from AddPalette

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 
 const styles = {
   root: {
@@ -9,15 +10,41 @@ const styles = {
     display: 'inline-block',
     position: 'relative',
     cursor: 'pointer',
-    marginBottom: '-3.5px'
+    marginBottom: '-3.5px',
+    "&:hover svg": {
+      color: 'white',
+      transform: 'scale(1.5)'
+    }
+  },
+  boxContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    width: '100%',
+    left: '0',
+    bottom: '0',
+    padding: '10px',
+    color: 'rgba(0,0,0,0.5)',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
+  removeIcon: {
+    transition: 'all 0.3s ease-in-out'
   }
 };
 
 const DraggableColorBox = props => {
-  const { classes } = props;
+  const { classes, handleClick, name, color } = props;
   return (
-    <div className={classes.root} style={{ backgroundColor: props.color }}>
-      {props.name || props.color}
+    <div
+      className={classes.root}
+      style={{ backgroundColor: color }}
+    >
+      <div className={classes.boxContent}>
+        <span>{name}</span>
+        <span><RemoveCircleIcon className={classes.removeIcon} onClick={handleClick}/></span>
+      </div>
     </div>
   );
 }
