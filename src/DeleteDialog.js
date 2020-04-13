3a69778d140624db395e8b40b6491278f1d68226
Lilly Oh <ohlilly@gmail.com>
2020-04-13 14:13:44 -0400

    added delete palette dialog to palette list

---
+++ src/DeleteDialog.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+
+const DeleteDialog = (props) => {
+  const { openDeleteDialog, toggleDialog, handleDelete } = props;
+  return (
+    <Dialog open={openDeleteDialog} aria-labelledby='delete-dialog-title' onClick={toggleDialog}>
+          <DialogTitle id='delete-dialog-title'>Delete this Palette?</DialogTitle>
+          <List>
+            <ListItem button onClick={handleDelete}>
+              <ListItemAvatar>
+                <Avatar style={{backgroundColor: blue[100], color: blue[600]}}>
+                  <CheckIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary='Delete' />
+            </ListItem>
+            <ListItem button onClick={toggleDialog}>
+              <ListItemAvatar>
+              <Avatar style={{backgroundColor: red[100], color: red[600]}}>
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary='Cancel' />
+            </ListItem>
+          </List>
+        </Dialog>
+  )
+}
+
+export default DeleteDialog;
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,17 +3,39 @@ import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+import DeleteDialog from './DeleteDialog';
 import MiniPalette from './MiniPalette';
 
 import styles from './styles/PaletteListStyles';
 
 class PaletteList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDeleteDialog: false,
+      deletingId: ''
+    }
+  }
+
   goToPalette = id => {
     this.props.history.push(`/palette/${id}`);
   }
 
+  toggleDialog = id => {
+    this.setState({
+      openDeleteDialog: !this.state.openDeleteDialog,
+      deletingId: id
+    });
+  }
+
+  handleDelete = (e) => {
+    e.persist();
+    this.props.deletePalette(this.state.deletingId);
+    this.toggleDialog();
+  }
+
   render() {
-    const { palettes, classes, deletePalette } = this.props;
+    const { palettes, classes } = this.props;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -28,13 +50,18 @@ class PaletteList extends React.Component {
                     {...palette}
                     key={palette.id}
                     id={palette.id}
-                    handleDelete={deletePalette}
+                    openDialog={this.toggleDialog}
                     handleClick={() => this.goToPalette(palette.id)}
                   />
                 </CSSTransition>
               ))}
             </TransitionGroup>
         </div>
+        <DeleteDialog
+          openDeleteDialog={this.state.openDeleteDialog}
+          toggleDialog={this.toggleDialog}
+          handleDelete={this.handleDelete}
+        />
       </div>
     )
   }
